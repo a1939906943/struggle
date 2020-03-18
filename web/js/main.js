caf3f308d47a664644ba231789b3e403560a1049
胡冰冰 <1939906943@qq.com>
2020-03-18 11:51:05 +0800

    jjj

--- web/js/main.js
+++ web/js/main.js
@@ -1,5 +1,10 @@
+// 后台项目地址
 var URL = "http://localhost:8081/";
-var obj = {};
+
+// 封装ajax
+// req: controller请求地址
+// data: 发送的数据 没有不传
+// success：成功回调函数
 function callServer({ req, data, success}) {
   $.post({
     url: URL + req + "?req=" + JSON.stringify(data),
